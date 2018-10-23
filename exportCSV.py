94197f8dcab75ba05f7dc43182f4429595b90792
nikos <nickarad94@gmail.com>
2018-10-23 18:55:36 +0300

    Add export.py

---
+++ exportCSV.py
@@ -0,0 +1,21 @@
+# https://github.com/mondejar/ecg-classification.git
+
+from os import listdir, mkdir, system
+from os.path import isfile, isdir, join, exists
+
+dir = 'mitdb'#'mitdb/'
+#Create folder
+dir_out = dir + 'csv/'
+if not exists(dir_out):
+	mkdir(dir_out)
+
+records = [f for f in listdir(dir) if isfile(join(dir, f)) if(f.find('.dat') != -1)]
+#print records
+
+for r in records:
+
+	command = 'rdsamp -r ' + dir + r[:-4] + ' -c -H -f 0 -v >' + dir_out + r[:-4] + '.csv'
+	print(command)
+	system(command)
+
+system(command_annotations)
--- plot.py
+++ plot.py
@@ -1,16 +1,22 @@
 # Documentation: https://wfdb.readthedocs.io/en/latest/plot.html
 import wfdb
 
-record = wfdb.rdrecord('mitdb/100', sampto=3000)
-ann = wfdb.rdann('mitdb/100', 'atr', sampto=3000)
+record = wfdb.rdrecord('mitdb/100', sampto=5000)
+ann = wfdb.rdann('mitdb/100', 'atr', sampto=5000)
 
+# wfdb.plot_items(signal=record.p_signal,
+#                 ann_samp=[ann.sample, ann.sample],
+#                 title='MIT-BIH Record 100', time_units='seconds',
+#                 figsize=(10,4), ecg_grids='all')
 wfdb.plot_items(signal=record.p_signal,
                 ann_samp=[ann.sample, ann.sample],
-                title='MIT-BIH Record 100', time_units='seconds',
-                figsize=(10,4), ecg_grids='all')
+                title='MIT-BIH Record 100',
+                figsize=(10,4)
+                )
 
 # wfdb.plot_wfdb(record=record, annotation=ann, plot_sym=True,
 #                time_units='seconds', title='MIT-BIH Record 100',
 #                figsize=(10,4), ecg_grids='all')
 
+# --> Plot all wfdb records in a directory (by finding header files), one at a time, until the ‘enter’ key is pressed.
 # wfdb.plot.plot_all_records(directory='mitdb')
